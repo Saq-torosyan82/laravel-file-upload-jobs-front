52b6259d279ba4fb4d1f8553bd615a969f11902d
Saqo <saqo.torosyan82@gmail.com>
2022-11-21 19:39:50 +0400

    made progress bar updating dynamic

--- pages/upload.js
+++ pages/upload.js
@@ -5,52 +5,68 @@ export default function Upload() {
     const fileRef = useRef()
     const API_URL = 'http://localhost:8000/api';
 
-    useEffect(() => {
-        processDetails();
-    }, []);
-
     const [processDetail, setProcessDetail] = useState({})
+    const [batchId, setBatchId] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        if (batchId != null) {
+            updateProgress();
+        }
+    }, [batchId]);
 
     function handleForm(e) {
-        e.preventDefault()
-        const inputFile = fileRef.current;
-        const file = inputFile.files[0];
+        e.preventDefault();
 
-        if (file) {
-            const formData = new FormData();
-            formData.append('mycsv', file)
-            fetch(`${API_URL}/upload`, { method: 'post', body: formData })
-                .then(res => res.json())
-                .then(data => console.log(data))
+        if (!loading) {
+            const inputFile = fileRef.current;
+            const file = inputFile.files[0];
+
+            if (file) {
+                const formData = new FormData();
+                formData.append('mycsv', file);
+                setLoading(true);
+
+                fetch(`${API_URL}/upload`, { method: 'post', body: formData })
+                    .then(res => res.json())
+                    .then(data => {
+                        setBatchId(data.id);
+                        processDetails();
+                        setLoading(false);
+                    })
+            }
         }
     }
 
-    function processDetails() {
-        const batchId = '97cc653c-79bf-495c-a3c5-214422b3243e';
-        fetch(`${API_URL}/batch?id=${batchId}`)
+    function processDetails(id = null) {
+        const selectedBatchId = id ?? batchId;
+
+        fetch(`${API_URL}/batch?id=${selectedBatchId}`)
             .then(res => res.json())
             .then(data => setProcessDetail(data))
     }
 
+    function updateProgress() {
+        setInterval(() => {
+            processDetails();
+        }, 2000);
+    }
+
     return (
         <Layout>
-            {processDetail &&
-                <section>
-                    <p>Upload is in progress</p>
+            { processDetail.progress >= 0 ?
+                (<section>
+                    <p>Upload is in progress ({processDetail.progress}%)</p>
                     <progress value={processDetail.progress} max="100"></progress>
-                </section>
-            }
-
-            {!processDetail &&
-                <section>
+                </section>) :
+                (<section>
                     <p className="text-xl text-gray-800 text-center mb-5">Choose a file to Upload</p>
                     <form className="border rounded p-4" onSubmit={handleForm}>
                         <input type="file" ref={fileRef} />
-                        <button type="submit" className="px-4 py-2 bg-gray-800 rounded text-white">Upload</button>
+                        <button type="submit" className={`px-4 py-2 rounded text-white ${loading ? 'bg-gray-400 otline-none' : 'bg-gray-800'}`}>Upload</button>
                     </form>
-                </section>
+                </section>)
             }
-
         </Layout>
     )
 }
