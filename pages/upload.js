43d95db4cdd91a98124b1ddcfe60d92717f24a47
Saqo <saqo.torosyan82@gmail.com>
2022-11-21 18:54:03 +0400

    created uploading progress bar

--- pages/upload.js
+++ pages/upload.js
@@ -1,9 +1,15 @@
-import React, {useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Layout from "../components/layout";
 
 export default function Upload() {
     const fileRef = useRef()
     const API_URL = 'http://localhost:8000/api';
+
+    useEffect(() => {
+        processDetails();
+    }, []);
+
+    const [processDetail, setProcessDetail] = useState({})
 
     function handleForm(e) {
         e.preventDefault()
@@ -13,19 +19,38 @@ export default function Upload() {
         if (file) {
             const formData = new FormData();
             formData.append('mycsv', file)
-            fetch(`${API_URL}/upload`, formData)
+            fetch(`${API_URL}/upload`, { method: 'post', body: formData })
                 .then(res => res.json())
                 .then(data => console.log(data))
         }
     }
 
+    function processDetails() {
+        const batchId = '97cc653c-79bf-495c-a3c5-214422b3243e';
+        fetch(`${API_URL}/batch?id=${batchId}`)
+            .then(res => res.json())
+            .then(data => setProcessDetail(data))
+    }
+
     return (
         <Layout>
-            <p className="text-xl text-gray-800 text-center mb-5">Choose a file to Upload</p>
-            <form className="border rounded p-4" onSubmit={handleForm}>
-                <input type="file" ref={fileRef} />
-                <button type="submit" className="px-4 py-2 bg-gray-800 rounded text-white">Upload</button>
-            </form>
+            {processDetail &&
+                <section>
+                    <p>Upload is in progress</p>
+                    <progress value={processDetail.progress} max="100"></progress>
+                </section>
+            }
+
+            {!processDetail &&
+                <section>
+                    <p className="text-xl text-gray-800 text-center mb-5">Choose a file to Upload</p>
+                    <form className="border rounded p-4" onSubmit={handleForm}>
+                        <input type="file" ref={fileRef} />
+                        <button type="submit" className="px-4 py-2 bg-gray-800 rounded text-white">Upload</button>
+                    </form>
+                </section>
+            }
+
         </Layout>
     )
 }
